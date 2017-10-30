555032d26beb0c0bd81e8fec0dc858cfe35a4d72
Jonathan Karlsson <karlsjona@gmail.com>
2017-10-30 23:02:31 +0100

    add vector algebra functions, rays

---
+++ src/main.rs
@@ -0,0 +1,188 @@
+extern crate rand;
+
+use std::ops::Mul;
+use std::ops::Add;
+
+fn main() {
+    let cam = Camera {
+        look_from: Point {x: 0.0, y: 1.0, z: -10.0},
+        direction: Vector {x: 0.0, y: 0.0, z: 1.0},
+        up: Vector {x:0.0, y: 1.0, z: 1.0},
+        fov: 1.0,
+        aspect: 1.0,
+    };
+    let r = generate_ray(cam);
+    println!("{}", r.origin.x);
+    println!("{}", r.origin.y);
+    println!("{}", r.origin.z);
+    println!("{}", r.direction.x);
+    println!("{}", r.direction.y);
+    println!("{}", r.direction.z);
+}
+
+struct Ray {
+    origin: Point,
+    direction: Vector,
+}
+
+impl Ray {
+    fn create(origin: Point, through: Point) -> Ray {
+        let direction = displacement(origin, through).normalize();
+        Ray {origin: origin, direction: direction}
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Point {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+#[derive(Copy, Clone)]
+struct Vector {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vector {
+    fn normalize(self) -> Vector
+    {
+        let l = self.length();
+        Vector{x: self.x / l, y: self.y, z: self.z}
+    }
+    fn length(self) -> f64
+    {
+        self.square_length().sqrt()
+    }
+    fn square_length(self) -> f64
+    {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+}
+
+impl Add for Vector {
+    type Output = Vector;
+
+    fn add(self, rhs: Vector) -> Vector {
+        Vector {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Mul<Vector> for f64 {
+    type Output = Vector;
+
+    fn mul(self, rhs: Vector) -> Vector {
+        Vector {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+struct Sphere {
+    centre: Point,
+    radius: f64,
+}
+
+struct Plane {
+    point: Point,
+    normal: Vector,
+}
+
+struct Camera {
+    look_from: Point,
+    direction: Vector,
+    up: Vector,
+    fov: f64,
+    aspect: f64,
+}
+
+fn displacement(p1: Point, p2: Point) -> Vector {
+    Vector {x: p2.x - p1.x, y: p2.y - p1.y, z: p2.z - p1.z}
+}
+
+fn translate(p: Point, v: Vector) -> Point {
+    Point {x: p.x + v.x, y: p.y + v.y, z: p.z + v.z }
+}
+
+fn cross(v1: Vector, v2: Vector) -> Vector {
+    Vector {
+        x: v1.y * v2.z - v1.z * v2.y,
+        y: v1.z * v2.x - v1.x * v2.z,
+        z: v1.x * v2.y - v1.y * v1.x
+    }
+}
+
+fn generate_ray(cam: Camera) -> Ray {
+    let origin = cam.look_from;
+    let p_orig = translate(origin, cam.direction);
+    let up = cam.up;
+    let left = cross(up, cam.direction);
+    let lr_range = (cam.fov / 2.0).tan();
+    let ud_range = lr_range / cam.aspect;
+    let p_x = lr_range * (-1.0 + 2.0 * rand::random::<f64>());
+    let p_y = ud_range * (-1.0 + 2.0 * rand::random::<f64>());
+    let p_disp = p_y * up + p_x * left;
+    let through = translate(p_orig, p_disp);
+    Ray::create(origin, through)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn displacement_test() {
+        let p1 = Point {x: 1.0, y: 2.0, z: 3.0};
+        let p2 = Point {x: 2.0, y: 4.0, z: 6.0};
+        let v = displacement(p1, p2);
+        assert_eq!(1.0, v.x);
+        assert_eq!(2.0, v.y);
+        assert_eq!(3.0, v.z);
+    }
+
+    #[test]
+    fn length_test() {
+        let v = Vector{x: 1.0, y: 2.0, z: 3.0};
+        let res = v.length();
+        // Reasonable eps?
+        assert!((1.0_f64 + 2.0_f64 + 3.0_f64).sqrt() - res < 1e-10);
+    }
+
+    #[test]
+    fn translate_test() {
+        let p = Point {x: 1.0, y: 2.0, z: 3.0};
+        let v = Vector {x: 4.0, y: 5.0, z: 6.0};
+        let res = translate(p, v);
+        assert_eq!(5.0, res.x);
+        assert_eq!(7.0, res.y);
+        assert_eq!(9.0, res.z);
+    }
+
+    #[test]
+    fn cross_test_1() {
+        let v1 = Vector{x: 1.0, y: 0.0, z: 0.0};
+        let v2 = Vector{x: 0.0, y: 1.0, z: 0.0};
+        let res = cross(v1,v2);
+        assert_eq!(0.0, res.x);
+        assert_eq!(0.0, res.y);
+        assert_eq!(1.0, res.z);
+    }
+
+    #[test]
+    fn cross_test_2() {
+        let v1 = Vector{x: 0.0, y: 0.0, z: 1.0};
+        let v2 = Vector{x: 0.0, y: 0.0, z: 1.0};
+        let res = cross(v1,v2);
+        assert_eq!(0.0, res.x);
+        assert_eq!(0.0, res.y);
+        assert_eq!(0.0, res.z);
+    }
+}
